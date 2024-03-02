3012b90034bd73c83b111cccbb77a140f1303149
rezy3312 <rezy3312@yahoo.com>
2024-03-02 08:45:59 -0600

    Added color Layout

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Skills from "./components/Skills/skills";
 import Works from "./components/Works/works";
 import Contact from "./components/Contact/contact";
 import Footer from "./components/Footer/footer"
+import "../src/App.css"
 
 
 function App() {
